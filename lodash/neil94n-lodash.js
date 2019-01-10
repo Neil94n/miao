ce195a87f6e18b5b068fdac7ae16b8533a0ac9da
rookie <417095066@qq.com>
2019-01-10 11:36:14 +0800

    add doc

---
+++ lodash/neil94n-lodash.js
@@ -0,0 +1,35 @@
+var neil94n = {
+    chunk: function chunk(ary, size = 1) {
+        var result = []
+        for(var i = 0; i< ??; i++) {
+          result.push(ary.slice(i, i + size))
+        }
+        return result
+      },
+      compact: function(ary) {
+        var result = []
+        for(var item of ary) {
+          if (item) {
+            result.push(item)
+          }
+        }
+        return result
+      }
+    }
+    
+    
+    var neil94n = function(){
+    
+      function chunk(ary, size = 1) {
+    
+      }
+      function compact(ary) {
+    
+      }
+    
+      return {
+        chunk: chunk,
+        compact: compact,
+      }
+    }()
+    
