788b5954b13a4467466a227f5a50b125b022b0d9
rookie <417095066@qq.com>
2019-01-10 19:25:17 +0800

    change

--- lodash/neil94n-lodash.js
+++ lodash/neil94n-lodash.js
@@ -1,35 +1,34 @@
 var neil94n = {
-    chunk: function chunk(ary, size = 1) {
-        var result = []
-        for(var i = 0; i< ??; i++) {
-          result.push(ary.slice(i, i + size))
-        }
-        return result
-      },
-      compact: function(ary) {
-        var result = []
-        for(var item of ary) {
-          if (item) {
-            result.push(item)
-          }
-        }
-        return result
+  chunk: function chunk(ary, size = 1) {
+    var result = []
+    for(var i = 0; i< ary.length; i++) {
+      result.push(ary.slice(i, i + size))
+    }
+    return result
+  },
+  compact: function(ary) {
+    var result = []
+    for(var item of ary) {
+      if (item) {
+        result.push(item)
       }
     }
-    
-    
-    var neil94n = function(){
-    
-      function chunk(ary, size = 1) {
-    
-      }
-      function compact(ary) {
-    
-      }
-    
-      return {
-        chunk: chunk,
-        compact: compact,
-      }
-    }()
-    
+    return result
+  }
+}
+
+
+var neil94n = function(){
+
+  function chunk(ary, size = 1) {
+
+  }
+  function compact(ary) {
+
+  }
+
+  return {
+    chunk: chunk,
+    compact: compact,
+  }
+}()
