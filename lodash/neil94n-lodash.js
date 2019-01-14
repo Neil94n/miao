9bcdd6dc3c45d9421ef83e299dd103348899b690
rookie <417095066@qq.com>
2019-01-14 16:53:12 +0800

    update

--- lodash/neil94n-lodash.js
+++ lodash/neil94n-lodash.js
@@ -30,6 +30,35 @@ var neil94n = {
         }
       }
       return result
+    },
+    difference: function difference(array, ...value){
+      let map = {} // 申请一个对象
+      let result = []
+      for (let a of values) {//遍历参数
+        if (Array.isArray(a)) {//判断参数是否为数组
+          for (let s of a) {//遍历数组 放进对象
+            if (!map[s]) {
+              map[s] = 1
+            }
+          }
+        } else if (!map[s]) { //遍历参数放进对象
+          map[s] = 1
+        }
+      }
+      for (let n of array) { //数组所给数组 返回对象中没有的属性
+        if (!map[n]) {
+          result.push(n)
+        }
+      }
+      return result
+    },
+    drop:function drop(array,n=1){
+      if(n>=array.length){
+        return[]
+      }
+      for (let i = 0; i < n; i++){
+        array.shift()
+      }
+      return array
     }
-
   }
